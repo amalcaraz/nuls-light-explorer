12060a88e99a30d383b4e7d13a1d83d1bfea0a30
Angel Manzano <angel.manzano.contractor@bbva.com>
2019-02-16 04:48:04 +0100

    Some fixes

--- environment/entry.sh
+++ environment/entry.sh
@@ -1,31 +1,34 @@
 #!/usr/bin/env bash
 # set -e
 
-npm install
-
 if [ "$1" = 'dev' ]
 then
 
+    npm install
     npm run serve:dev
 
 elif [ "$1" = 'test' ]
 then
 
+    npm install
     npm run test:watch
 
 elif [ "$1" = 'coverage' ]
 then
 
+    npm install
     npm run coverage
 
 elif [ "$1" = 'lint' ]
 then
 
+    npm install
     npm run lint:fix
 
 elif [ "$1" = 'docs' ]
 then
 
+    npm install
     npm run docs  
 
 elif [ "$1" = 'prod' ]
